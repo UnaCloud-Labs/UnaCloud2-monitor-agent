f67b8314fe21514705c9cbe0eed162d808a26f23
Nicolás Gómez <n.gomez22@uniandes.edu.co>
2018-09-23 23:44:55 -0500

    Implement report generation in CSV files

--- agent/monitor.py
+++ agent/monitor.py
@@ -21,6 +21,7 @@ DICT_ATTRS = [
     "memory_info",
     "memory_full_info"
 ]
+STATS_LIST = []
 
 
 def get_process_stats():
@@ -31,5 +32,24 @@ def get_process_stats():
     for thread in stats["threads"]:
         threads.append(dict(thread._asdict()))
     stats["threads"] = threads
+    STATS_LIST.append(stats)
     return stats
 
+def report():
+    avg_memory_file = open("reports/avg_memory.csv", "a")
+    avg_cpu_file = open("reports/avg_cpu.csv", "a")
+    memory_file = open("reports/memory.csv", "a")
+    cpu_file = open("reports/cpu.csv", "a")
+
+    memory_percent_list = [stat["memory_percent"] for stat in STATS_LIST]
+    cpu_percent_list = [stat["cpu_percent"] for stat in STATS_LIST]   
+    avg_memory_percent = average(memory_percent_list)
+    avg_cpu_percent = average(cpu_percent_list)
+
+    avg_memory_file.write(avg_memory_percent)
+    avg_cpu_file.write(avg_cpu_percent)
+    memory_file.write(str.join(memory_percent_list, ","))
+    cpu_file.write(str.join(cpu_percent_list, ","))
+
+def average(l):
+    return sum(l) / len(l)
