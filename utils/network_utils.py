4215ce518be04aafa1b3cb61bb430a34fe77a137
Nicolás Gómez <ngomezgg.18@gmail.com>
2018-11-07 14:43:47 -0500

    Improve IP and RTT setting process

--- utils/network_utils.py
+++ utils/network_utils.py
@@ -10,6 +10,7 @@ import helpers.data_helper as dh
 class NetworkUtils:
 
     RTT = 1
+    IP = None
 
     def __init__(self, rtt_freq):
         self.rtt_freq = rtt_freq
@@ -20,15 +21,25 @@ class NetworkUtils:
 
     def set_rtt(self, thread_name):
         while (True):
+            print("pinging")
             start_time = time()
             ping_proc = subprocess.Popen("ping 157.253.236.113", stdout=subprocess.PIPE)
             ping_output = ping_proc.stdout.read().decode('utf-8')
             average_regex = r'Average = ([0-9]+)ms'
-            self.RTT = re.search(average_regex, ping_output).group(1)
+            new_rtt = re.search(average_regex, ping_output).group(1)
+            if new_rtt:
+                self.RTT = int(new_rtt)
+            else:
+                self.RTT = -1
             sleep(self.rtt_freq - ((time() - start_time)))
 
     def get_ip_addr(self):
-        return urlopen('http://ip.42.pl/raw').read().decode("utf-8")
+        if(not self.IP):
+            self.set_ip_addr()
+        return self.IP
+
+    def set_ip_addr(self):
+        self.IP = urlopen('http://ip.42.pl/raw').read().decode("utf-8")
 
     def get_net_stats(self):
         stats = psutil.net_if_stats()
